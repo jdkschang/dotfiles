e3d50b40bccb9c1d50aac98855895aa2446fc1ff
Jonathan D Chang <jdkschang@gmail.com>
2018-09-11 14:15:27 -0700

    updating ui config

--- dots/emacs/doom/+ui.el
+++ dots/emacs/doom/+ui.el
@@ -1,19 +1,39 @@
 ;;; ~/dotfiles/dots/emacs/doom/+ui.el -*- lexical-binding: t; -*-
 
-;; Themes
-(setq doom-themes-enable-bold t
-	  doom-themes-enable-italic t)
+;; natural title bar
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
 
-;; (load-theme 'doom-nord t)
-;; (load-theme 'doom-opera t)
-(load-theme 'doom-city-lights t)
+;; Line Numbers
+(setq display-line-numbers 'relative)
+;; terminal mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+							  (interactive)
+							  (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+							  (interactive)
+							  (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t))
 
-;; Fonts
-(setq doom-font (font-spec :family "Operator Mono SSm" :size 16))
-;; On my (almost) 1440p monitor, 28pt Hack gives me about 30 lines and maybe
-;; 100-120 columns, which seems like a good zoomed in size for eye
-;; strain/showing other people stuff
-(setq doom-big-font (font-spec :family "Operator Mono SSm" :size 28))
+;; Theme
+;; (setq doom-theme 'doom-nord)
+;; (setq doom-theme 'doom-city-lights)
+;; (setq doom-theme 'doom-opera)
+;; (setq doom-theme 'doom-dracula)
+
+(setq doom-font (font-spec :family "Operator Mono SSm" :size 16)
+      doom-big-font (font-spec :family "Operator Mono SSm" :size 28)
+      doom-variable-pitch-font (font-spec :family "Operator Mono SSm" :slant 'italic)
+      doom-unicode-font (font-spec :family "Dank Mono"))
+
+(defun my-theme-customizations ()
+  (set-face-italic 'font-lock-comment-face t))
+
+(add-hook 'doom-load-theme-hook #'my-theme-customizations)
 
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))

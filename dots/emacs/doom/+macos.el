a2ec89596f02b997b82c540a070bc36e0381c627
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2018-10-24 10:58:11 -0700

    added gruvbox theme; updating emacs titlebar color to match dark

--- dots/emacs/doom/+macos.el
+++ dots/emacs/doom/+macos.el
@@ -7,7 +7,8 @@
 (when (eq system-type 'darwin)       ; should be the same as (when IS-MAC...)
   (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
   (setq dired-use-ls-dired nil)
+
   ;; macOS natural title bars
   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . light)))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)))
 

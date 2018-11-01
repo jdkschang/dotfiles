a7607596b46667e816fe4f0e957dab19920be9ff
Jonathan Chang <6375175+jdkschang@users.noreply.github.com>
2018-10-31 20:37:13 -0700

    debugging +ui.el render error

--- dots/emacs/doom/+ui.el
+++ dots/emacs/doom/+ui.el
@@ -1,9 +1,5 @@
 ;;; ~/dotfiles/dots/emacs/doom/+ui.el -*- lexical-binding: t; -*-
 
-;;;;;
-;;;;; User Interface
-;;;;;
-
 ;; Setting transparency values
 (defvar solid 100)
 (defvar transparent 88)
@@ -12,15 +8,15 @@
 (defun +jdkschang/toggle-transparency ()
   (interactive)
   (cond
-    ((eq (frame-parameter nil 'alpha) solid)
-		 (message "Toggling to transparent: %s" transparent)
-		 (set-frame-parameter nil 'alpha transparent))
+   ((eq (frame-parameter nil 'alpha) solid)
+	(message "Toggling to transparent: %s" transparent)
+	(set-frame-parameter nil 'alpha transparent))
 
-		((eq (frame-parameter nil 'alpha) transparent)
-		 (message "Toggling to solid: %s" solid)
-		 (set-frame-parameter nil 'alpha solid))
+   ((eq (frame-parameter nil 'alpha) transparent)
+	(message "Toggling to solid: %s" solid)
+	(set-frame-parameter nil 'alpha solid))
 
-		(t (message "Can't toggle transparency."))))
+   (t (message "Can't toggle transparency."))))
 
 (set-frame-parameter nil 'alpha transparent)
 
@@ -28,17 +24,17 @@
 (setq display-line-numbers-type 'relative)
 
 ;; terminal mouse support
-(unless window-system
-  (require 'mouse)
-  (xterm-mouse-mode t)
-  (global-set-key [mouse-4] (lambda ())
-                (interactive)
-                (scroll-down 1))
-  (global-set-key [mouse-5] (lambda ())
-                (interactive)
-                (scroll-up 1))
-  (defun track-mouse (e))
-  (setq mouse-sel-mode t))
+;; (unless window-system
+;;   (require 'mouse)
+;;   (xterm-mouse-mode t)
+;;   (global-set-key [mouse-4] (lambda ())
+;;                 (interactive)
+;;                 (scroll-down 1))
+;;   (global-set-key [mouse-5] (lambda ())
+;;                 (interactive)
+;;                 (scroll-up 1))
+;;   (defun track-mouse (e))
+;;   (setq mouse-sel-mode t))
 
 ;; Font
 (setq doom-font (font-spec :family "Operator Mono SSm Lig" :size 16)

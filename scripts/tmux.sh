a868c055208fd472eb3a0f221e2d93f83d27177b
Jonathan Chang <6375175+jdkschang@users.noreply.github.com>
2018-11-12 22:31:26 -0800

    adding in tmux.sh

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,19 @@
-tmux new-session -d -s config
-tmux send-keys 'df' 'C-m'
-tmux rename-window 'config'
-tmux select-window -t config:1
-tmux split-window -v
-tmux send-keys ''
+#!/bin/sh
+tmux new-session -d -s base -n config
+  tmux send-keys 'df' C-m
+  tmux split-window -h
 
+tmux new-window -n main
+  tmux send-keys 'proj' C-m
+  tmux split-window -h
+  tmux send-keys 'proj' C-m
+  tmux split-window -v
+  tmux send-keys 'proj' C-m
+
+tmux new-session -d -s gc -n geesee
+  tmux send-keys 'rwa' C-m
+  tmux split-window -h
+  tmux send-keys 'rwa' C-m
+  tmux send-keys 'y start' C-m
+  tmux split-window -v
+  tmux send-keys 'rwa' C-m

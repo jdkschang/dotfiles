364cd8c37aeb72beceea3bd5a85c43c30f3b5756
Jonathan Chang <6375175+jdkschang@users.noreply.github.com>
2018-12-19 22:23:03 -0800

    added flags for llvm compiler; added move-text and fancy org bullets

--- emacs/doom/+bindings.el
+++ emacs/doom/+bindings.el
@@ -9,12 +9,14 @@
    (:prefix "a"
      :desc "Ranger" :n "r" #'ranger
      :desc "Deer" :n "d" #'deer)
+   ;; origami folds
    (:prefix "z"
      :desc "origami-recursively-toggle-node" :nv "z" #'origami-recursively-toggle-node
      :desc "origami-recursively-toggle-node" :i "C-z" #'origami-recursively-toggle-node
      :desc "origami-toggle-node" :nv "z" #'origami-toggle-node
      :desc "origami-toggle-node" :i "C-Z" #'origami-toggle-node
      :desc "origami-show-only-node" :nv "A-z" #'origami-show-only-node))
+
  ;; toggle transparency
  (:gnvime "M-u" #'+jdkschang/toggle-transparency)
 

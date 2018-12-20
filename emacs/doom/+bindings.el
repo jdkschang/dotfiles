5c5cb4f84181fc2453a9ea06d19f51cf51e2e983
Jonathan Chang <6375175+jdkschang@users.noreply.github.com>
2018-12-19 20:40:03 -0800

    adding origami mode

--- emacs/doom/+bindings.el
+++ emacs/doom/+bindings.el
@@ -7,8 +7,14 @@
      :desc "toggle-theme" :n "t" #'+jdkschang/toggle-theme)
    ;; ranger
    (:prefix "a"
-		 :desc "Ranger" :n "r" #'ranger
-		 :desc "Deer" :n "d" #'deer))
+     :desc "Ranger" :n "r" #'ranger
+     :desc "Deer" :n "d" #'deer)
+   (:prefix "z"
+     :desc "origami-recursively-toggle-node" :nv "z" #'origami-recursively-toggle-node
+     :desc "origami-recursively-toggle-node" :i "C-z" #'origami-recursively-toggle-node
+     :desc "origami-toggle-node" :nv "z" #'origami-toggle-node
+     :desc "origami-toggle-node" :i "C-Z" #'origami-toggle-node
+     :desc "origami-show-only-node" :nv "A-z" #'origami-show-only-node))
  ;; toggle transparency
  (:gnvime "M-u" #'+jdkschang/toggle-transparency)
 

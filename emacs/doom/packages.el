34ee395f74bdd33b0cb29338ac252ff4307721b3
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2018-12-27 11:08:29 -0800

    fixing matchit autoloads error

--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -23,3 +23,7 @@
 (package! origami)
 (package! move-text)
 (package! org-fancy-priorities)
+
+
+;; Fixing matchit autoloads error
+(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4")

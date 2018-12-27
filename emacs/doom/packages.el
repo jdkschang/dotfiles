41005d55ed200d9a84e5b3d79dea8e33ae9c5fa3
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2018-12-27 11:12:37 -0800

    adding extra paren

--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -26,4 +26,4 @@
 
 
 ;; Fixing matchit autoloads error
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4")
+(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))

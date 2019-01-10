54224ceea055034038e4d71c46316a8a6f3f476c
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2019-01-10 14:41:33 -0800

    reverting to base setting

--- emacs/doom/+macos.el
+++ emacs/doom/+macos.el
@@ -4,32 +4,41 @@
 ;;;; macOS specific settings
 ;;;;
 
-(defmacro help/on-mac-os (statement &rest statements)
-  "Evaluate the enclosed body only when run on macOS."
-  `(when (eq system-type 'darwin)
-     ,statement
-     ,@statements))
+; (defmacro help/on-mac-os (statement &rest statements)
+;   "Evaluate the enclosed body only when run on macOS."
+;   `(when (eq system-type 'darwin)
+;      ,statement
+;      ,@statements))
 
-;; (when (eq system-type 'darwin)       ; should be the same as (when IS-MAC...)
-;;   (setq exec-path (append exec-path '("/Users/jchang/.nvm/versions/node/v8.11.4/bin")))
+(when (eq system-type 'darwin)       ; should be the same as (when IS-MAC...)
+   (setq exec-path (append exec-path '("/Users/jchang/.nvm/versions/node/v8.11.4/bin")))
+   (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
+   (setq dired-use-ls-dired nil)
 
-;;   (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
-;;   (setq dired-use-ls-dired nil)
+   ;; macOS natural title bars
+   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+   (add-to-list 'default-frame-alist '(ns-appearance . dark)))
 
-;;   ;; macOS natural title bars
-;;   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-;;   (add-to-list 'default-frame-alist '(ns-appearance . dark)))
-(help/on-mac-os
- (use-package exec-path-from-shell
-  :ensure t
-  :config
-  ;; thinner strokes for font-smoothing
-  (setq ns-use-thin-smoothing t)
-  (setq dired-use-ls-dired nil)
-  ;; macOS natural title bars
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . dark))
+; (help/on-mac-os
+;  (use-package exec-path-from-shell
+;   :ensure t
+;   :config
+;   (setq exec-path-from-shell-check-startup-files nil)
+;   (setq exec-path-from-shell-debug 1)
+;   (exec-path-from-shell-initialize)))
 
-  (setq exec-path-from-shell-check-startup-files nil)
-  (setq exec-path-from-shell-debug 1)
-  (exec-path-from-shell-initialize)))
+; (help/on-mac-os
+;   ;; thinner strokes for font-smoothing
+;   (setq ns-use-thin-smoothing t)
+;   (setq dired-use-ls-dired nil)
+;   ;; macOS natural title bars
+;   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+;   (add-to-list 'default-frame-alist '(ns-appearance . dark)))
+
+; (help/on-mac-os
+;  (defun help/yes-or-no-p (orig-fun &rest args)
+;    "Prevent yes-or-no-p from activating a dialog."
+;    (let ((use-dialog-box nil))
+;      (apply orig-fun args)))
+;  (advice-add #'yes-or-no-p :around #'help/yes-or-no-p)
+;  (advice-add #'y-or-n-p :around #'help/yes-or-no-p))

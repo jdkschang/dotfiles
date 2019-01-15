beecefc0e25a6ed01969392f0dbffb14fa33baf1
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2019-01-15 15:39:10 -0800

    better mac detection; inspired by hlissner's personal dotfiles

--- emacs/doom/+macos.el
+++ emacs/doom/+macos.el
@@ -10,7 +10,7 @@
 ;      ,statement
 ;      ,@statements))
 
-(when (eq system-type 'darwin)       ; should be the same as (when IS-MAC...)
+(when IS-MAC       ; should be the same as (when IS-MAC...)
    (setq exec-path (append exec-path '("/Users/jchang/.nvm/versions/node/v8.11.4/bin")))
    (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
    (setq dired-use-ls-dired nil)
@@ -18,6 +18,7 @@
    ;; macOS natural title bars
    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
    (add-to-list 'default-frame-alist '(ns-appearance . dark)))
+   ;; (add-hook 'window-setup-hook #'toggle-frame-maximized))
 
 ; (help/on-mac-os
 ;  (use-package exec-path-from-shell

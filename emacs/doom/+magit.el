de9c2000239fa101ae3964e54ba3adc2248bacc8
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2019-01-15 15:39:53 -0800

    catching up with hlissner updates;

--- emacs/doom/+magit.el
+++ emacs/doom/+magit.el
@@ -1,16 +1,16 @@
 ;;; ~/dotfiles/dots/emacs/doom/+magit.el -*- lexical-binding: t; -*-
 
-(setq +magit-hub-features t ;; I want the PR/issue stuff too!
-      +magit-hub-enable-by-default t)  ;; And I want it on by default!
-
 (after! magit
-  (magit-wip-after-save-mode t)
-  (magit-wip-after-apply-mode t)
-  (setq magit-save-repository-buffers 'dontask
-        magit-repository-directories '(("~/Dropbox/Projects/" . 3)
-									   ("~/Projects/" . 3)
-                                       ("~/dotfiles/" . 0))
-        magit-popup-display-buffer-action nil ;; Not sure why this is here, wonder what it does
-        magit-display-file-buffer-function #'switch-to-buffer-other-window
-        magithub-clone-default-directory "~/Dropbox/Projects")) ;; I want my stuff to clone to ~/projects
-
+	(magit-wip-after-save-mode t)
+	(magit-wip-after-apply-mode t)
+	(setq magit-save-repository-buffers nil
+				magit-repository-directories '(("~/Dropbox/Projects/" . 3)
+																			 ("~/Projects/" . 3)
+																			 ("~/dotfiles/" . 0))
+				magit-commit-arguments '("--gpg-sign=D1BDD1F5AA60F93E")
+				magit-rebase-arguments '("--autostash" "--gpg-sign=D1BDD1F5AA60F93E")
+				magit-pull-arguments   '("--rebase" "--autostash" "--gpg-sign=D1BDD1F5AA60F93E")
+				magit-display-file-buffer-function #'switch-to-buffer-other-window
+				magithub-clone-default-directory "~/Dropbox/Projects" ;; I want my stuff to clone to ~/projects
+				+magit-hub-features t ;; I want the PR/issue stuff too!
+				+magit-hub-enable-by-default t))  ;; And I want it on by default!
--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/dotfiles/dots/emacs/doom/packages.el
 
-;; Misc stuff
-;; Can highlight a region and hit M-x carbon-now-sh to get a https://carbon.now
-;; of the region
-(package! carbon-now-sh)
+;; ivy
+(package! ivy-yasnippet)
+
+(package! golden-ratio)
 
 ;; JS stuff
 (package! prettier-js)
@@ -16,14 +16,7 @@
 ;; Filestuff
 (package! ranger)
 
-;; Reasonml stuff
-(package! reason-mode)
-
 ;; Better folds
 (package! origami)
 (package! move-text)
-(package! org-fancy-priorities)
 
-
-;; Fixing matchit autoloads error
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))

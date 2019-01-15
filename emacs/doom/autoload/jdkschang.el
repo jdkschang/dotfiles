83dc3507ed05870d30ab152d51a04ba1cac1cf49
Jonathan D Chang <6375175+jdkschang@users.noreply.github.com>
2019-01-15 15:33:18 -0800

    adding in "personal" autoload functions
    - inspired by
      - [[https://github.com/Emiller88][Edmund M]]
      - [[https://github.com/hlissner][Henrik Lissner]]

---
+++ emacs/doom/autoload/jdkschang.el
@@ -0,0 +1,33 @@
+;;; ~/dotfiles/emacs/doom/autoload/jdkschang.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +jdkschang/find-in-dotfiles ()
+	"Open a file somewhere in ~/dotfiles via a fuzzy filename search."
+	(interactive)
+	(doom-project-find-file (expand-file-name "~/dotfiles")))
+
+;;;###autoload
+(defun +jdkschang/browse-dotfiles ()
+	"Browse the files in ~/dotfiles."
+	(interactive)
+	(doom-project-browse (expand-file-name "~/dotfiles")))
+
+;; (defun +jdkschang/find-notes-for-major-mode (&optional arg)
+;;   "TODO"
+;;   (interactive "P")
+;;   (let ((default-directory (expand-file-name "code/" +org-dir)))
+;;     (if arg
+;;         (call-interactively #'find-file)
+;;       (find-file
+;;        (expand-file-name (concat (string-remove-suffix "-mode" (symbol-name major-mode)) ".org"))))))
+
+;;;###autoload
+(defun +jdkschang/find-notes-for-project (&optional arg)
+	"TODO"
+	(interactive "P")
+	(let ((project-root (doom-project-name 'nocache))
+				(default-directory (expand-file-name "projects/" +org-dir)))
+		(if arg
+				(call-interactively #'find-file)
+			(find-file
+			 (expand-file-name (concat project-root ".org"))))))

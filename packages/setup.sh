ad9ee379acebf9f302a2fb3e29cd2ec01ee1b24c
Jonathan D. Chang <6375175+jdkschang@users.noreply.github.com>
2019-07-29 21:54:01 -0700

    getting v3 ready

---
+++ packages/setup.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env sh
+
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+echo "directory: $DIR"
+
+. ~/projects/jdkschang/dotfiles/scripts/functions.sh
+
+COMMENT=\#*
+
+sudo -v
+
+info "Installing Brewfile packages..."
+brew bundle
+success "Finished installing Brewfile packages."
+
+fnm use 11
+success "Switched to Node v11"
+
+find * -name "*.list" | while read fn; do
+    cmd="${fn%.*}"
+    set -- $cmd
+    info "Installing $1 packages..."
+    while read package; do
+        if [[ $package == $COMMENT ]];
+        then continue
+        fi
+        substep_info "Installing $package..."
+        $cmd $package
+    done < "$fn"
+    success "Finished installing $1 packages."
+done

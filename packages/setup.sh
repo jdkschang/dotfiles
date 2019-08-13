6174f251258d99d3e40fef363a9a8c41dad6b038
Jonathan D. Chang <jchang@guidedchoice.com>
2019-08-13 11:57:06 -0700

    debugging brewfile

--- packages/setup.sh
+++ packages/setup.sh
@@ -17,16 +17,16 @@ success "Finished installing Brewfile packages."
 fnm use 11
 success "Switched to Node v11"
 
-find * -name "*.list" | while read fn; do
-    cmd="${fn%.*}"
-    set -- $cmd
-    info "Installing $1 packages..."
-    while read package; do
-        if [[ $package == $COMMENT ]];
-        then continue
-        fi
-        substep_info "Installing $package..."
-        $cmd $package
-    done < "$fn"
-    success "Finished installing $1 packages."
-done
+# find * -name "*.list" | while read fn; do
+#     cmd="${fn%.*}"
+#     set -- $cmd
+#     info "Installing $1 packages..."
+#     while read package; do
+#         if [[ $package == $COMMENT ]];
+#         then continue
+#         fi
+#         substep_info "Installing $package..."
+#         $cmd $package
+#     done < "$fn"
+#     success "Finished installing $1 packages."
+# done

6c9d97eff6fbad37d3e2e8197980cd2af6c0e9a7
Jonathan D. Chang <jdkschang@apple.com>
2020-02-06 10:49:16 -0800

    Try Iosvkem theme

--- doom/+macos.el
+++ doom/+macos.el
@@ -8,7 +8,6 @@
   (setq ns-use-thin-smoothing t) ; thinner strokes for font-smoothing
   (setq dired-use-ls-dired nil)
   (setq ns-auto-titlebar-mode t)
-  (add-hook 'window-setup-hook #'toggle-frame-maximized)
   ;; macOS natural title bars
   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
   (add-to-list 'default-frame-alist '(ns-appearance . dark)))

a57805941cb00a836907a4b475252a027dabb92f
Jonathan D. Chang <jdkschang@apple.com>
2020-02-17 10:34:04 -0800

    Fix magit prefix settings

--- doom/+magit.el
+++ doom/+magit.el
@@ -6,8 +6,9 @@
         ;; don't restore the wconf after quitting magit
         magit-inhibit-save-previous-winconf t
         transient-values '((magit-commit "--gpg-sign=50C3D91E4C96CE4A")
-                           (magit-rebase "--autosquash" "--gpg-sign=50C3D91E4C96CE4A")
-                           (magit-pull   "--rebase" "--gpg-sign=50C3D91E4C96CE4A"))
+                           (magit-fetch  "--prune")
+                           (magit-rebase "--autostash" "--interactive" "--gpg-sign=50C3D91E4C96CE4A")
+                           (magit-pull   "--autostash" "--rebase" "--gpg-sign=50C3D91E4C96CE4A"))
         +magit-hub-features t ;; I want the PR/issue stuff too!
         +magit-hub-enable-by-default t))  ;; And I want it on by default!
 

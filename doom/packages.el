b5c4385bf9261c14395554a7442eb65916c59eb4
Jonathan D. Chang <jdkschang@apple.com>
2020-04-06 17:44:27 -0700

    Add auto tpm (tmux plugin manager) install
    - https://github.com/tmux-plugins/tpm/blob/master/docs/automatic_tpm_installation.md
    - update set -g @plugin:
      https://github.com/tmux-plugins/tpm/blob/master/docs/tpm_not_working.md#help-tpm-not-working
    
    Add abbreviations for work

--- doom/packages.el
+++ doom/packages.el
@@ -19,8 +19,3 @@
 (package! zetteldeft)
 
 (package! git-timemachine)
-;; (use-package lsp-python-ms
-;;   :ensure t
-;;   :hook (python-mode . (lambda ()
-                         ;; (require 'lsp-python-ms)
-                         ;; (lsp))))

f01e0e87cb8986e6c4234c0ce115fa20f4d991ab
Jonathan D. Chang <jdkschang@apple.com>
2020-04-26 13:30:31 -0700

    :sparkles: Add ssh init to tmux init process

--- tmux/scripts/tmux.fish
+++ tmux/scripts/tmux.fish
@@ -6,6 +6,7 @@ function session_base
     tmux new-session -d -s base
     tmux neww -k -n config -t base:1
     tmux send-keys 'dfl' C-m
+    tmux send-keys 'git status' C-m
     tmux split-window -h
     tmux send-keys 'cd ~/.emacs.d' C-m
     tmux send-keys 'doom sync & doom build' C-m
@@ -27,6 +28,8 @@ function session_base
     tmux send-keys 'popb' C-m
     tmux send-keys 'pap' C-m
     tmux send-keys './manage.py runserver' C-m
+
+    sshadd
 end
 
 function has_session -d 'check if session exists'
@@ -38,6 +41,12 @@ function except -d 'if has session errored then run session init script'
     if [ $status -eq 1 ]
         $argv[1]
     end
+end
+
+function sshadd -d 'add ssh config'
+    echo 'add ssh profiles'
+    ssh-add -K ~/.ssh/apple
+    ssh-add -K ~/.ssh/github-personal
 end
 
 #MAIN

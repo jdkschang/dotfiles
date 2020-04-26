d88b7b28292d518ae7bb2447e4af3946bfbc3216
Jonathan D. Chang <jdkschang@apple.com>
2020-04-26 12:31:43 -0700

    :sparkles: Automate tmux base session
    - :construction: Build three tmux windows
      - config (emacs, dotfiles)
      - frontend (population)
      - backend (population)
    - :sparkles: Add abbreviation to start tmux init base session command
    - :poop: Attach to tmux instance, but in a forloop

--- tmux/scripts/tmux.fish
+++ tmux/scripts/tmux.fish
@@ -2,45 +2,48 @@
 
 set SESSIONS base
 
-# function session_gc
-#   tmux new-session -d -s gc
-#   tmux neww -k -n geesee -t gc:1
-#   tmux send-keys 'rwa' C-m
-#   tmux split-window -h
-#   tmux send-keys 'rwa' C-m
-#   tmux send-keys 'yarn' C-m
-#   tmux send-keys 'yarn start' C-m
-#   tmux split-window -v
-#   tmux send-keys 'rwa' C-m
-# end
-
 function session_base
-  tmux new-session -d -s base
-  tmux neww -k -n config -t base:1
-  tmux send-keys 'dfl' C-m
-  tmux split-window -h
-  tmux send-keys 'cd ~/.emacs.d' C-m
-  tmux neww -k -n main -t base:2
-  tmux send-keys 'proj' C-m
-  tmux split-window -h
-  tmux send-keys 'proj' C-m
-  tmux split-window -v
-  tmux send-keys 'proj' C-m
+    tmux new-session -d -s base
+    tmux neww -k -n config -t base:1
+    tmux send-keys 'dfl' C-m
+    tmux split-window -h
+    tmux send-keys 'cd ~/.emacs.d' C-m
+    tmux send-keys 'doom sync & doom build' C-m
+
+    tmux neww -k -n pop-front -t base:2
+    tmux send-keys 'popf' C-m
+    tmux send-keys 'git status' C-m
+    tmux split-window -h
+    tmux send-keys 'popf' C-m
+    tmux send-keys 'npm start' C-m
+
+    tmux neww -k -n pop-back -t base:3
+    tmux send-keys 'popb' C-m
+    tmux send-keys 'git status' C-m
+    tmux split-window -h
+    tmux send-keys 'popb' C-m
+    tmux send-keys 'docker-compose up' C-m
+    tmux split-window -v
+    tmux send-keys 'popb' C-m
+    tmux send-keys 'pap' C-m
+    tmux send-keys './manage.py runserver' C-m
 end
 
 function has_session -d 'check if session exists'
-  tmux has -t $argv[1] ^ /dev/null
+    echo 'check if session exists: ' $argv[1]
+    tmux has-session $argv[1] ^/dev/null
 end
 
 function except -d 'if has session errored then run session init script'
-  if [ $status -eq 1 ]
-    $argv[1]
-  end
+    if [ $status -eq 1 ]
+        $argv[1]
+    end
 end
 
 #MAIN
 for x in $SESSIONS
-  echo $x
-  has_session $x
-  except session_$x
+    echo $x
+    has_session $x
+    except session_$x
+    tmux attach -t $x
 end
